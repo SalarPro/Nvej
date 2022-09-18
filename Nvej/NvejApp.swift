e6911bc7716580a5fd1b00f4baf598be3cef333d
Salar Pro <salarkhalid97@gmail.com>
2022-09-19 00:26:19 +0300

    Initial Commit

---
+++ Nvej/NvejApp.swift
@@ -0,0 +1,17 @@
+//
+//  NvejApp.swift
+//  Nvej
+//
+//  Created by Salar Pro on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct NvejApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
